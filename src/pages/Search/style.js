89c9f9eb766391c1ab09bad9178f89070ac1124d
JeongHwanJi <nawhgnoejij@naver.com>
2022-03-22 18:12:53 +0900

    rename (Search folder) : change css

--- src/pages/Search/style.js
+++ src/pages/Search/style.js
@@ -1,326 +1,9 @@
 import styled from "styled-components";
 
 //image
-import { noti_icon } from "../../assets/images/image";
+import { noti_icon, search_icon } from "../../assets/images/image";
 
 export const SDetailWrap = styled.div`
-  h3 {
-    margin: 100px 0px 10px 20px;
-    text-align: start;
-    font-weight: 700;
-    font-size: 16px;
-    color: #282828;
-  }
-
-  p {
-    margin: 60px 0px 10px 20px;
-    text-align: center;
-  }
-
-  button {
-    margin: 10px 0px 10px 10px;
-    width: 70px;
-    height: 32px;
-    border-radius: 15px;
-    font-weight: 400;
-    font-size: 13px;
-  }
-
-  .benny {
-    margin: 10px 0px 10px 10px;
-    width: 87px;
-    height: 32px;
-    border-radius: 15px;
-    font-weight: 400;
-    font-size: 13px;
-  }
-
-  .the {
-    margin: 10px 0px 10px 10px;
-    width: 115px;
-    height: 32px;
-    border-radius: 15px;
-    font-weight: 400;
-    font-size: 13px;
-  }
-
-  .address {
-    margin: 40px 0px 10px 20px;
-    text-align: center;
-  }
-
-  .gangnam {
-    margin: 10px 0px 10px 10px;
-    width: 115px;
-    height: 32px;
-    border-radius: 15px;
-    font-weight: 400;
-    font-size: 13px;
-  }
-
-  .place {
-    margin: 40px 0px 10px 20px;
-    text-align: center;
-  }
-
-  .garosu {
-    margin: 10px 10px 10px 10px;
-    width: 80px;
-    height: 32px;
-    border-radius: 15px;
-    font-weight: 400;
-    font-size: 13px;
-  }
-
-  .culture {
-    margin: 10px 10px 10px 10px;
-    width: 160px;
-    height: 32px;
-    border-radius: 15px;
-    font-weight: 400;
-    font-size: 13px;
-  }
-
-  .container {
-    width: 364px;
-    height: 138px;
-    display: flex;
-    flex-direction: column;
-    background: #f5f5f5;
-    margin: 10px auto 10px auto;
-    border-radius: 10px;
-  }
-
-  .emptySpace {
-    width: 390px;
-    height: 50px;
-    background-color: white;
-    margin: auto;
-  }
-
-  .commend {
-    color: #282828;
-    font-size: 19px;
-    font-weight: bold;
-    margin-left: 15px;
-    margin-top: 15px;
-  }
-
-  .oneList {
-    display: flex;
-    /* justify-content: space-between; */
-    /* width: max-content;  */
-    height: 32px;
-    margin-left: 15px;
-    margin-top: 15px;
-    border-radius: 10px;
-  }
-
-  .one_one {
-    /* width: max-content; */
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-right: 10px;
-    border-radius: 50px;
-    background: rgba(255, 103, 158, 0.1);
-    color: #ff679e;
-    font-size: 15px;
-    /* background: rgba(255, 103, 158, 0.1); */
-    padding: 10px;
-  }
-
-  .one_two {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(255, 103, 158, 0.1);
-    color: #ff679e;
-    border-radius: 50px;
-    font-size: 15px;
-    padding: 10px;
-  }
-
-  .one_three {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(255, 103, 158, 0.1);
-    color: #ff679e;
-    border-radius: 50px;
-    padding: 10px;
-    margin-left: 10px;
-    font-size: 15px;
-  }
-
-  .one_four {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(255, 103, 158, 0.1);
-    color: #ff679e;
-    border-radius: 50px;
-    padding: 10px;
-    margin-left: 10px;
-    font-size: 15px;
-  }
-
-  .one_five {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(255, 103, 158, 0.1);
-    color: #ff679e;
-    border-radius: 50px;
-    padding: 10px;
-    margin-left: 10px;
-    font-size: 15px;
-  }
-
-  .two_one {
-    /* width: max-content; */
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-right: 10px;
-    border-radius: 50px;
-    color: #9c9cdc;
-    background: rgba(156, 156, 220, 0.1);
-    /* background: rgba(255, 103, 158, 0.1); */
-    padding: 10px;
-    font-size: 15px;
-  }
-
-  .two_two {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(156, 156, 220, 0.1);
-    color: #9c9cdc;
-    border-radius: 50px;
-    padding: 10px;
-    font-size: 15px;
-  }
-
-  .two_three {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(156, 156, 220, 0.1);
-    color: #9c9cdc;
-    border-radius: 50px;
-    padding: 10px;
-    margin-left: 10px;
-    font-size: 15px;
-  }
-
-  .two_four {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(156, 156, 220, 0.1);
-    color: #9c9cdc;
-    border-radius: 50px;
-    padding: 10px;
-    margin-left: 10px;
-    font-size: 15px;
-  }
-
-  .two_five {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(156, 156, 220, 0.1);
-    color: #9c9cdc;
-    border-radius: 50px;
-    padding: 10px;
-    margin-left: 10px;
-    font-size: 15px;
-  }
-
-  .twoList {
-    display: flex;
-    /* justify-content: space-between; */
-    margin-top: 10px;
-    margin-left: 5px;
-    height: 32px;
-  }
-
-  .three_one {
-    /* width: max-content; */
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-right: 10px;
-    border-radius: 50px;
-    color: #777777;
-    background: #ededed;
-    /* background: rgba(255, 103, 158, 0.1); */
-    padding: 10px;
-    font-size: 15px;
-  }
-
-  .three_two {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #777777;
-    background: #ededed;
-    border-radius: 50px;
-    padding: 10px;
-    font-size: 15px;
-  }
-
-  .three_three {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #777777;
-    background: #ededed;
-    border-radius: 50px;
-    padding: 10px;
-    margin-left: 10px;
-    font-size: 15px;
-  }
-
-  .three_four {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #777777;
-    background: #ededed;
-    border-radius: 50px;
-    padding: 10px;
-    margin-left: 10px;
-    font-size: 15px;
-  }
-
-  .three_five {
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #777777;
-    background: #ededed;
-    border-radius: 50px;
-    padding: 10px;
-    margin-left: 10px;
-    font-size: 15px;
-  }
 `;
 
 export const HeaderWrap = styled.div`
@@ -328,109 +11,131 @@ export const HeaderWrap = styled.div`
   padding-top: 40px;
   background-color: #f9c9c9;
   display: flex;
-  position: relative;
-  justify-content: space-between;
-
-  .search_wrap {
-    display: flex;
-    width: 85%;
-    border-radius: 10px;
-    border: 1px solid #ebebeb;
-    background-color: #fff;
-    align-items: center;
-  }
-
-  select {
-    border-top-left-radius: 10px;
-    border-bottom-left-radius: 10px;
-    border: none;
-    background-color: #fff;
-    color: #777777;
-  }
-
-  input {
-    width: 75%;
-    padding: 8px 10px;
-    box-sizing: border-box;
-    border: none;
-    border-right: 1px solid #ebebeb;
-    border-left: 1px solid #ebebeb;
-    background-color: #fff;
-    color: #777777;
-  }
-
-  .search {
-    color: #777777;
-    margin: 0 10px;
-  }
-
-  .notice_wrap {
-    display: felx;
-    align-items: center;
-    justify-content: center;
-    width: 35px;
-    height: 35px;
-    border-radius: 35px;
-    border: 1px solid #ebebeb;
-    background-color: #fff;
-  }
-`;
-
-export const SearchWrap = styled.div`
-  padding: 20px;
-  padding-top: 40px;
-  background-color: #f9c9c9;
-  display: flex;
   justify-content: space-between;
   position: relative;
   align-items: center;
-  select {
-    border-top-left-radius: 10px;
-    border-bottom-left-radius: 10px;
-    border: none;
-    background-color: #fff;
-    color: #777777;
-  }
-  .search_wrap {
-    height: 3.5rem;
-    display: flex;
-    align-items: center;
-    flex: auto;
-    margin-right: 10px;
-    background-color: #fff;
-  }
+`;
 
-  .search {
-    color: #777777;
-  }
+export const SearchWrap = styled.div`
+  height: 3.5rem;
+  display: flex;
+  align-items: center;
+  flex: auto;
+  margin-right: 10px;
+  background-color: #fff;
+`;
 
-  input {
-    padding: 8px 10px;
-    box-sizing: border-box;
-    border: none;
-    color: #777777;
-    outline: none;
-  }
-  .search {
-    padding-left: 1rem;
-  }
-  .notice_wrap {
-    display: felx;
-    align-items: center;
-    justify-content: center;
-    width: 35px;
-    height: 35px;
-    border-radius: 35px;
-    border: 1px solid #ebebeb;
-    background-color: #fff;
-  }
+export const Select = styled.select`
+  text-align: center;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+  border: none;
+  background-color: #fff;
+  color: #777777;
+`;
 
-  .bell {
-    margin-top: 0.85rem;
-  }
+export const Option = styled.option`
+`;
+
+export const Input = styled.input`
+  height: 35px;
+  font-size: 13px;
+  box-sizing: border-box;
+  border: none;
+  color: #777777;
+  outline: none;
+  padding-left: 10px;
+`;
+
+export const SearchIcon = styled.img.attrs({
+  src: `${search_icon}`,
+  alt: "search_icon",
+})`margin-left: 50px;
 `;
 
 export const Bell = styled.img.attrs({
   src: `${noti_icon}`,
   alt: "noti_icon",
-})``;
+})`margin-top: 0.85rem;`;
+
+export const EmptySpace = styled.div`
+  width: 390px;
+  height: 8vh;
+  background-color: white;
+  margin: auto; 
+`;
+
+export const Container = styled.div`
+  width: 364px;
+  height: 138px;
+  display: flex;
+  flex-direction: column;
+  background: #f5f5f5;
+  margin: 10px auto 10px auto;
+  border-radius: 10px;
+`;
+
+export const CommendText = styled.h1`
+  color: #282828;
+  font-size: 19px;
+  font-weight: bold;
+  margin-left: 15px;
+  margin-top: 15px;
+`;
+
+export const PlaceList1 = styled.div`
+  display: flex;
+  height: 32px;
+  margin-left: 15px;
+  margin-top: 15px;
+  border-radius: 10px;
+`
+
+export const PlaceList2 = styled.div`
+  display: flex;
+  margin-top: 10px;
+  margin-left: 15px;
+  height: 32px;
+`;
+
+export const Place = styled.div`
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+  border-radius: 50px;
+  background: rgba(255, 103, 158, 0.1);
+  color: #ff679e;
+  font-size: 15px;
+  padding: 10px;
+`;
+
+export const Place2 = styled.div`
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+  border-radius: 50px;
+  background: rgba(156, 156, 220, 0.1);
+  color: #9C9CDC;
+  font-size: 15px;
+  padding: 10px;
+`;
+
+export const Place3 = styled.div`
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+  border-radius: 50px;
+  background: #EDEDED;
+  color: #777777;
+  font-size: 15px;
+  padding: 10px;
+`;
+
+
+
