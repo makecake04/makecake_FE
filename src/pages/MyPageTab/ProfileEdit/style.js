d0e9c4536012a4d4b90d2d29db483585a364370f
durukim <kimduru@naver.com>
2022-03-23 16:05:50 +0900

    rename (ProfileEdit) : change css

--- src/pages/MyPageTab/ProfileEdit/style.js
+++ src/pages/MyPageTab/ProfileEdit/style.js
@@ -1,102 +1,110 @@
 import styled from "styled-components";
 
 //image
-import { profile_image } from "../../../assets/images/image";
+import { profile_image, black_back_button } from "../../../assets/images/image";
 
-export const ProfileWrap = styled.div`
-  .title {
-    padding: 40px 0px 20px 30px;
-    display: flex;
-    align-items: center;
-  }
-  .left {
-    color: #646464;
-    margin: 0px 80px 0px 0px;
-  }
+export const ProfileWrap = styled.div``;
 
+export const SubWrap = styled.div``;
+
+export const Header = styled.header`
+  padding: 2rem 0;
+  display: flex;
+  align-items: center;
   h3 {
+    margin-left: 21%;
+    vertical-align: middle;
     font-weight: 700;
     font-size: 19px;
     color: #282828;
   }
+`;
 
-  hr {
-    border: 0.7px solid #e5e5e5;
-    width: 100%;
-  }
+export const BlackBackButton = styled.img.attrs({
+  src: `${black_back_button}`,
+  alt: "back-button",
+})`
+  margin-left: 3%;
+`;
 
-  .profile_img {
-    width: 100px;
-    height: 100px;
-    margin: 50px auto 20px auto;
-    border-radius: 100px;
-    background-color: #ddd;
-    background-image: url(${profile_image});
-    background-position: center;
-    background-size: 100px;
-  }
+export const HrWrap = styled.hr`
+  border: 0.7px solid #e5e5e5;
+  width: 100%;
+`;
 
-  .nickname {
-    text-align: center;
-    font-weight: 700;
-    font-size: 16px;
-    color: #282828;
-    margin: 0 auto 5px auto;
-  }
+export const ContentWrap = styled.div``;
 
-  .email {
-    text-align: center;
-    font-weight: 400;
-    font-size: 14px;
-    color: #646464;
-    margin: 0 auto 40px auto;
-  }
+export const ProfileImage = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 50px auto 20px auto;
+  border-radius: 100px;
+  background-color: #ddd;
+  background-image: url(${profile_image});
+  background-position: center;
+  background-size: 100px;
+`;
 
-  .bold {
-    border: 3px solid #f7f7f7;
-    width: 100%;
-  }
+export const NicknameP = styled.p`
+  text-align: center;
+  font-weight: 700;
+  font-size: 16px;
+  color: #282828;
+  margin: 0 auto 5px auto;
+`;
 
-  .content {
-    display: flex;
-    margin: 30px 30px;
-    align-items: center;
-  }
+export const EmailP = styled.p`
+  text-align: center;
+  font-weight: 400;
+  font-size: 14px;
+  color: #646464;
+  margin: 0 auto 40px auto;
+`;
 
-  .contents {
-    font-weight: 400;
-    font-size: 16px;
-    color: #282828;
-    margin: 0px 30px 0px 0px;
-  }
+export const BoldHr = styled.hr`
+  border: 3px solid #f7f7f7;
+  width: 100%;
+`;
 
-  button {
-    width: 50px;
-    height: 30px;
-    border: 1px solid #eaeaea;
-    font-size: 13px;
-    margin: 0px 0px 0px 16rem;
-  }
+export const Content = styled.div`
+  display: flex;
+  margin: 30px 30px;
+  align-items: center;
+`;
 
-  .profile_hr {
-    border: 0.7px solid #eaeaea;
-    width: 90%;
-    margin: 0 auto;
-  }
+export const ContentP = styled.p`
+  font-weight: 400;
+  font-size: 16px;
+  color: #282828;
+  margin: 0px 30px 0px 0px;
+`;
 
-  .btn_wrap {
-    display: flex;
-    justify-content: end;
-  }
+export const ProfileHr = styled.hr`
+  border: 0.7px solid #eaeaea;
+  width: 90%;
+  margin: 0 auto;
+`;
 
-  .signout {
-    width: 80px;
-    height: 20px;
-    margin: 20px 20px 0px 0px;
-    color: #777;
-    font-size: 13px;
-    border: none;
-  }
+export const EditButton = styled.button`
+  width: 50px;
+  height: 30px;
+  border: 1px solid #eaeaea;
+  font-size: 13px;
+  margin: 0px 0px 0px 16rem;
+`;
+
+export const ButtonWrap = styled.div`
+  display: flex;
+  justify-content: end;
+`;
+
+export const SignoutButton = styled.button`
+  width: 80px;
+  height: 20px;
+  margin: 20px 20px 0px 0px;
+  color: #777;
+  font-size: 13px;
+  border: none;
 `;
 
 export const ModalWrap = styled.div`
@@ -104,63 +112,63 @@ export const ModalWrap = styled.div`
   flex-direction: column;
   text-align: center;
   align-items: center;
+`;
 
-  .title {
-    color: #292929;
-    font-weight: 700;
-    margin: 20px 0px 10px 0px;
-  }
+export const TitleP = styled.p`
+  color: #292929;
+  font-weight: 700;
+  margin: 20px 0px 10px 0px;
+`;
 
-  .description {
-    color: #292929;
-    font-size: 15px;
-    margin: 0px 0px 20px 0px;
-  }
+export const DescriptionP = styled.p`
+  color: #292929;
+  font-size: 15px;
+  margin: 0px 0px 20px 0px;
+`;
 
-  .modal_input {
-    width: 250px;
-    height: 50px;
-    border-radius: 20px;
-    background-color: #f5f5f5;
-    border: 1px solid #fff;
-  }
+export const ModalInput = styled.input`
+  width: 250px;
+  height: 50px;
+  border-radius: 20px;
+  background-color: #f5f5f5;
+  border: 1px solid #fff;
+`;
 
-  .modal_hr {
-    border: 0.7px solid #e5e5e5;
-    width: 100%;
-    margin: 20px 0px 10px 0px;
-  }
+export const ModalHr = styled.hr`
+  border: 0.7px solid #e5e5e5;
+  width: 100%;
+  margin: 20px 0px 10px 0px;
+`;
 
-  .footer_wrap {
-    display: flex;
-  }
+export const EditBtn = styled.button`
+  color: #ff679e;
+  font-weight: 700;
+  border: none;
+  width: 140px;
+`;
 
-  .footer {
-    color: #ff679e;
-    font-weight: 700;
-    border: none;
-    width: 140px;
-  }
+export const FooterWrap = styled.div`
+  display: flex;
+`;
 
-  .footer_one {
-    color: #ff679e;
-    font-weight: 700;
-    border: none;
-    width: 140px;
-  }
+export const BackButton = styled.button`
+  color: #ff679e;
+  font-weight: 700;
+  border: none;
+  width: 140px;
+`;
 
-  .vl {
-    border-left: 2px solid #e5e5e5;
-    height: 41px;
-    position: absolute;
-    left: 50%;
-    bottom: 0;
-  }
+export const Vl = styled.div`
+  border-left: 2px solid #e5e5e5;
+  height: 41px;
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+`;
 
-  .footer_two {
-    color: #c6c6c8;
-    font-weight: 700;
-    width: 140px;
-    border: none;
-  }
+export const DropOutButton = styled.button`
+  color: #c6c6c8;
+  font-weight: 700;
+  width: 140px;
+  border: none;
 `;
