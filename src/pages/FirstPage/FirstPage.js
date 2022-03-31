7ed3f181e3843602e1bcce09ca3d2563a71f24f8
JeongHwanJi <nawhgnoejij@naver.com>
2022-03-31 17:34:02 +0900

    fix (FirstPage) : fix

--- src/pages/FirstPage/FirstPage.js
+++ src/pages/FirstPage/FirstPage.js
@@ -67,7 +67,7 @@ const FirstPage = () => {
       <SocialLoginWrap>
         <KakaoLogin />
         <GoogleLogin />
-        {/* <NaverLogin /> */}
+        <NaverLogin />
       </SocialLoginWrap>
     </Container>
   );
